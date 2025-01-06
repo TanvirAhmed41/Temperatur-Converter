977848b3f833a1a91791d1d5b9c550391d253072
Tanvir <santoahmed225@gmail.com>
2025-01-06 11:28:06 +0600

    Fahrenheit to Celsius Converter done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Test {
+
+    public static void Main (string[] args){
+
+       double Fahrenheit, celsius ;
+
+       Console.WriteLine ("Temperature Calcutor");
+
+       Console.Write("Enter Fahrenheit Temperature: ");
+       Fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+       celsius = (Fahrenheit - 32)/1.8 ;
+
+       Console.WriteLine ($"Temperature in Celsius = {celsius.ToString("F2")}");
+
+
+    }
+
+
+}
